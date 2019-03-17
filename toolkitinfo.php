8d4b200d894e3d8d463f89b53b2a6822e981967a
Shawn McKee <smckee@umich.edu>
2019-03-17 14:44:57 -0400

    Fixed footer and custom div sections

--- toolkitinfo.php
+++ toolkitinfo.php
@@ -410,12 +410,12 @@
 Your selected perfSONAR Toolkit is: <b><?php echo $_GET["toolkits"]; ?></b>
 </nav>
 
-<div class="flex-container" style="min-height=200px;">
+<div class="flex-container" style="min-height=220px;">
  <div>
 <iframe id="ifr" name="ifr" style="float:right" src="https://psetf.opensciencegrid.org/etf/check_mk/view.py?filled_in=filter&host_regex=<?php echo $_GET["toolkits"]; ?>&selection=ff14182e-35bd-412e-a95a-526b80959b00&view_name=searchhost" height="220" width="500"></iframe><br>
  </div>
 
- <div style=="min-height=200px;">
+ <div style=="min-height=220px;">
 <h2>Customized Web links for <?php echo $_GET["toolkits"]; ?></h2>
 This toolkit's web page: 
 <a href="https://<?php echo $_GET["toolkits"]; ?>/toolkit">https://<?php echo $_GET["toolkits"]; ?>/toolkit</a> <br>
@@ -430,9 +430,10 @@ This toolkit's settings and status  <a href="https://<?php echo $_GET["toolkits"
   <footer>
         <a style="float: left;padding: 30px 10px;" href="mailto:wlcg-perfsonar-support@cern.ch?subject=ToolkitInfoFeedback">Contact about the webpage</a>
         <center>      
-        <a href="https://iris-hep.org/"><img src="img/Iris-hep-6-WHITE-complete.png" alt="The IRIS-HEP logo" width="200" height="80"></a> </center>
+        <a href="https://iris-hep.org/"><img src="img/Iris-hep-6-WHITE-complete.png" alt="The IRIS-HEP logo" width="200" height="80"></a>
         <a href="https://www.nsf.gov/"><img src="img/nsf_round_logo.png" alt="The NSF logo" width="95" height="95"></a>
  	</center>
+        <a style="float: right;padding: 30px 10px;" href="mailto:wlcg-perfsonar-support@cern.ch?subject=ToolkitInfoFeedback">Contact about the webpage</a>
   </footer>
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
