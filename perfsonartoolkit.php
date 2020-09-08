f2fc56017c75793ac820e1ae02277adb7fa2a12d
Thomas Shearer AGLT2 <shearert@umt3int05.aglt2.org>
2020-09-08 13:01:55 -0400

    testing lat and long

--- perfsonartoolkit.php
+++ perfsonartoolkit.php
@@ -400,6 +400,19 @@
         echo 'User Real IP - '.getUserIpAddr();
        ?>
  </div>
+ <div>
+   <?php
+     $ip_addr = $_SERVER['REMOTE_ADDR'];
+     $geoplugin = unserialize( file_get_contents('http://www.geoplugin.net/php.gp?ip='.$ip_addr) );
+
+     if ( is_numeric($geoplugin['geoplugin_latitude']) && is_numeric($geoplugin['geoplugin_longitude']) ) {
+
+     $lat = $geoplugin['geoplugin_latitude'];
+     $long = $geoplugin['geoplugin_longitude'];
+     }
+     echo $ip_addr.';'.$lat.';'.$long;
+    ?>
+ </div>
 
  <div id="custom-links" style="margin-left: 10px;vertical-align: top;display: none">
 Web links for <b><?php echo htmlspecialchars($_GET[toolkits]); ?><b><br>
