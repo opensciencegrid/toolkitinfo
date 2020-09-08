2eaa5983a0b0b59ce3e7006585c62f778ef15862
Thomas Shearer AGLT2 <shearert@umt3int05.aglt2.org>
2020-09-08 14:10:38 -0400

    testing function with user latlong

--- perfsonartoolkit.php
+++ perfsonartoolkit.php
@@ -424,7 +424,7 @@
 
  <div>
   <?php
-   echo distance(32.9697, -96.80322, 29.46786, -98.53506, "M") . " Miles<br>";
+   echo distance($lat, $long, 29.46786, -98.53506, "M") . " Miles<br>";
   ?>
  </div> 
 
