ebff319b5053fd779ac1dd74c6cc31f3940aecff
root <root@psrsv.opensciencegrid.org>
2020-11-06 13:42:03 -0500

    addition of the data type

--- perfsonartoolkit.php
+++ perfsonartoolkit.php
@@ -102,13 +102,13 @@
  </div>
  <div>
   <?php
-   $csv = array_map('str_getcsv', file('/etc/testpython/testhostscan.csv'));
+   $csv = array_map('str_getcsv', file('/etc/devtestpython/testhostscan.csv'));
    foreach($csv as $location => $data)
    {
     $calculated_distance = distance($lat,$long,$data[0],$data[1],"K");
     $distance_array[$location][distance] = $calculated_distance;
     $distance_array[$location][host_name] = $data[2];
-    $distance_array[$location][site_name] = $data[3];
+    $distance_array[$location][data_type] = $data[3];
    }
    $sorting = array_column($distance_array,'distance');
    array_multisort($sorting, SORT_ASC, $distance_array);
@@ -185,19 +185,26 @@
  </form>
 -->
 
+<?php
+ $search = array_column($distance_array,'host_name');
+ $samplehost = $_GET[host];
+ $key = array_search($samplehost,$search);
+?>
+
 
 </div>
 <p style="color:white">Your selected perfSONAR Toolkit Host is: <b><?php echo htmlspecialchars($_GET[host]); ?></b></p>
+<p style="color:white">Your selected perfSONAR Toolkit Host is: <b><?php echo $distance_array[$key]['data_type']; ?></b></p>
 <!--
 <p style="color:white">Your selected perfSONAR Toolkit Site is: <b><?php echo htmlspecialchars($_GET[site]); ?></b></p>
 -->
 
 </nav>
-
  <article>
- <div id='custom-links'>
+
+ <div id='both'>
  <h4> You have selected a toolkit host. If any of these issues are prevalent to your problem, please select them and your toolkit host will be both a source and destination </h4>
- <h4> NOTE: Some hosts may show no data when a link is clicked. This is because the host is for a different type of data. Please select a similar host or different data type </h4>
+ <h4> NOTE: The toolkit host you selected is both a throughput and latency host. Therefore, all links are displayed </h4>
  <div class='row'>
   <div class='column'>
    <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/82552400-d8cb-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20packetloss.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Packetloss%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Do You Suspect a lot of Packetloss When Transmitting Data to Another Site? </button></a>
@@ -210,10 +217,53 @@
    <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/57e3c0b0-d7f3-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20throughput.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Throughput%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Are You Seeing a lot of Throughput Issues for your Site?</button></a>
    <br> </br>
    <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/68ec3520-d8cc-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20retransmits.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Retransmits%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Are You Seeing a lot of Retransmitted Data for your Site?</button></a>
+  </div>
  </div>
  </div>
+
+ <div id='throughput'>
+ <h4> You have selected a toolkit host. If any of these issues are prevalent to your problem, please select them and your toolkit host will be both a source and destination </h4>
+ <h4> NOTE: The toolkit host you selected is a throughput host. Therefore, only links regarding throughput are displayed </h4>
+ <div class='row'>
+  <div class='column'>
+   <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/35976890-d8ca-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20trace.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Trace%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Are You Interested in the Path that Data Takes from your Site to Another?</button></a>
+   <br> </br>
+   <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/68ec3520-d8cc-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20retransmits.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Retransmits%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Are You Seeing a lot of Retransmitted Data for your Site?</button></a>
+  </div>
+  <div class='column'>
+   <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/57e3c0b0-d7f3-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20throughput.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Throughput%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Are You Seeing a lot of Throughput Issues for your Site?</button></a>
+   <br> </br>
+  </div>
+ </div>
+ </div>
+
+ <div id='latency'>
+ <h4> You have selected a toolkit host. If any of these issues are prevalent to your problem, please select them and your toolkit host will be both a source and destination </h4>
+ <h4> NOTE: The toolkit host you selected is a latency host. Therefore, only links regardling latency are displayed </h4>
+ <div class='row'>
+  <div class='column'>
+   <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/82552400-d8cb-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20packetloss.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Packetloss%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Do You Suspect a lot of Packetloss When Transmitting Data to Another Site? </button></a>
+   <br> </br>
+   <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/35976890-d8ca-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20trace.',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20Trace%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Are You Interested in the Path that Data Takes from your Site to Another?</button></a>
+   <br> </br>
+  </div>
+  <div class='column'>
+   <a href=https://atlas-kibana.mwt2.org:5601/s/networking/app/kibana#/dashboard/a1ce0000-d8d4-11ea-9344-2da4788d78a4?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-1d,to:now))&_a=(description:'This%20dashboard%20allows%20for%20the%20user%20to%20select%20both%20the%20source%20host,%20as%20well%20as%20any%20destination%20it%20may%20apply%20to,%20so%20to%20give%20all%20of%20the%20relevant%20information%20about%20the%20given%20parameters.%20This%20dashboard%20primarily%20focuses%20on%20the%20One-Way%20Delay.',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:b1029d00-e61d-11ea-9ef7-e1db34380ad3,key:dest_site,negate:!t,params:(query:IEPSAS-Kosice),type:phrase),query:(match_phrase:(dest_site:IEPSAS-Kosice))),('$state':(store:appState),meta:(alias:!n,disabled:!f,index:b1029d00-e61d-11ea-9ef7-e1db34380ad3,key:src_site,negate:!t,params:(query:IEPSAS-Kosice),type:phrase),query:(match_phrase:(src_site:IEPSAS-Kosice)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:'(src_host:<?php echo htmlspecialchars($_GET[host]); ?>%20AND%20dest_host:*)%20OR%20(src_host:*%20AND%20dest_host:<?php echo htmlspecialchars($_GET[host]); ?>)'),timeRestore:!f,title:'Site-Based%20perfsonar%20One-Way%20Delay%20Breakdown%20ts-ih',viewMode:view) target='_blank'><button class='block'>Do you Suspect Slow Connections between your Site and Another?</button></a>
+  </div>
+ </div>
+ </div>
+
+ <div id='neither'>
+ <h4> You have selected a toolkit host. If any of these issues are prevalent to your problem, please select them and your toolkit host will be both a source and destination </h4>
+ <div class='row'>
+  <div class='column'>
+    <h3> NOTE: The toolkit host you selected is neither a throughput nor latency host. Therefore, no links are displayed. Please choose another toolkit host and contact the owner of the toolkit host for more information</h3>
+  </div>
+ </div>
+ </div>
+
  </article>
- </div>
+
  <div id='no-cust-text'>
  <div class='row'>
   <div class='column'>
@@ -277,10 +327,37 @@
 	var host = "<?php echo htmlspecialchars($_GET[host]); ?>";
 	if ( typeof host === 'undefined' || host === null || host == "") {
            document.getElementById("no-cust-text").style.display="inline";
-           document.getElementById("custom-links").style.display="none";
+           document.getElementById("both").style.display="none";
+           document.getElementById("latency").style.display="none";
+           document.getElementById("throughput").style.display="none";
+           document.getElementById("neither").style.display="none";
 	} else {
-           document.getElementById("custom-links").style.display="inline";
-           document.getElementById("no-cust-text").style.display="none";
+           var data = "<?php echo $distance_array[$key]['data_type']; ?>";
+           if ( data === 'latency') {
+              document.getElementById("both").style.display="none";
+              document.getElementById("no-cust-text").style.display="none";
+              document.getElementById("latency").style.display="inline";
+              document.getElementById("throughput").style.display="none";
+              document.getElementById("neither").style.display="none";
+           } if ( data === 'throughput') {
+              document.getElementById("both").style.display="none";
+              document.getElementById("no-cust-text").style.display="none";
+              document.getElementById("latency").style.display="none";
+              document.getElementById("throughput").style.display="inline";
+              document.getElementById("neither").style.display="none";
+           } if ( data === 'neither') {
+              document.getElementById("both").style.display="none";
+              document.getElementById("no-cust-text").style.display="none";
+              document.getElementById("latency").style.display="none";
+              document.getElementById("throughput").style.display="none";
+              document.getElementById("neither").style.display="inline";
+           } if ( data === 'both') {
+              document.getElementById("both").style.display="inline";
+              document.getElementById("no-cust-text").style.display="none";
+              document.getElementById("latency").style.display="none";
+              document.getElementById("throughput").style.display="none";
+              document.getElementById("neither").style.display="none";
+           }
 	}
  </script>
   <footer>
